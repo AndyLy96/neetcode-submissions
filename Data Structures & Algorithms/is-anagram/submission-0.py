f69a09549287da415e12f7cd0d05ba66f9315bac
Andy Ly <104222070+AndyLy96@users.noreply.github.com>
2026-06-20 13:03:42 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sortedS = "".join(sorted(s))
+        sortedT = "".join(sorted(t))
+        if sortedS.lower() == sortedT.lower():
+            return True
+        return False
